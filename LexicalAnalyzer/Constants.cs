154111850f64a1b8c5fffeb777073703b5e1441b
jvlguanco <jvlguanco@gmail.com>
2024-02-25 12:56:52 +0800

    Updated Constants

--- LexicalAnalyzer/Constants.cs
+++ LexicalAnalyzer/Constants.cs
@@ -100,7 +100,7 @@ public class Constants
                                                 'A','B','C','D','E','F','G','H','I','J','K','L','M',
                                                 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         
-        public List<char> del10 = new List<char> { ' ', '\n', '}',
+        public List<char> del10 = new List<char> { ' ', '\n', '}', ';', ',',
                                                 'a','b','c','d','e','f','g','h','i','j','k','l','m',
                                                 'n','o','p','q','r','s','t','u','v','w','x','y','z',
                                                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
@@ -114,7 +114,7 @@ public class Constants
                                                 'A','B','C','D','E','F','G','H','I','J','K','L','M',
                                                 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         
-        public List<char> del12 = new List<char> { ' ', '~', '!',
+        public List<char> del12 = new List<char> { ' ', '~', '!', '{',
                                                 'a','b','c','d','e','f','g','h','i','j','k','l','m',
                                                 'n','o','p','q','r','s','t','u','v','w','x','y','z',
                                                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
@@ -153,7 +153,7 @@ public class Constants
     public class LiteralsDelims
     {
         public List<char> delim_txt = new List<char> { ' ', '\n', ';', ',', ')', '.', '<', '=', '[' };
-        public List<char> delim_num = new List<char> { '+', '-', '*', '/', '.', ' ', '\n', ';', '&', '|', ')', ',', '&', ']', '=', '>', '<', '!' };
+        public List<char> delim_num = new List<char> { '+', '-', '*', '/', '.', ' ', '\n', ';', '&', '|', ')', ',', '&', ']', '=', '>', '<', '!', '}' };
     }
     
     public class Identifier
